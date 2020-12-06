0f89d6518aa21aa3246c5a008b38485c6ef194a7
videogamerm <75590266+videogamerm@users.noreply.github.com>
2020-12-06 18:30:12 -0500

    Add files via upload

---
+++ h@lloween/Download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir h@lloween
+echo "1/7"
+cd h@lloween
+
+
+echo "2/7"
+mkdir Code
+echo "3/7"
+cd Code
+echo "4/7"
+wget mason.thewithers.org/h@ll0ween/Code/aTTACK.py
+echo "5/7"
+wget mason.thewithers.org/h@ll0ween/Code/start.py
+echo "6/7"
+wget mason.thewithers.org/h@ll0ween/Code/h@ll0ween.sh
+echo "7/7 Done!!!!"
